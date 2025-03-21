affde416a0f15a7e4b3b94897d60eb969c61b003
paw2kk <wndxakk@gmail.com>
2025-03-21 10:36:15 +0700

    menambahkan display

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -40,4 +40,26 @@ void insertionSort(){
     j--;//Step4b
     }
     arr[j] = temp;//Step 5
+}]
+
+void display(){
+    cout << endl;//Output baris Kosong
+    cout << "=================================" << endl;
+    cout << "Total Passs = " << n - 1 << endl; //Count element movement
+    cout << "=================================" << endl; //Output ke layar
+    cout << "Element Array yang telah tersusun" << endl; //Output ke layar
+    cout << "=================================" << endl; //Output ke layar
+for (int j = 0; j < n; j++){//Looping dengan j dimulai dari 0 hingga n-1
+    cout << arr[j] << endl;//Output ke layar
+}
+cout<< endl; //Output baris kosong
+}
+
+
+int main(){
+    input();
+    insertionSort();
+    display();
+    system("Pause");
+    return 0;
 }
