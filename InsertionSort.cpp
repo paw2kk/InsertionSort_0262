792f5d4bb40fb0deb6a20dff2ef476a3266c2d0e
paw2kk <wndxakk@gmail.com>
2025-03-21 10:08:54 +0700

    membuat input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -6,6 +6,27 @@ int n; //Membuat Variabel inputan n
 
 void input(){ //Produce Input
     while {true}
-}
+{
     cout << "Masukkan Jumlah Data pada Array : "; //Membuat Inputan jumlah element Array
-    cin >> n; //Memanggil variabel inputan pertama
+    cin >> n; //Memanggil variabel inputan n
+
+    if (n <= 20)
+    {//Membuat Kondisi n tidak lebih dari 20
+        break;
+    }
+    else 
+    {
+        cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; //Menampilkan Pesan jika lebih dari 20
+    }
+}
+cout << enl; //Membuat jarak per baris program
+cout << "======================" << endl;
+cout << "Masukkan Element Array" << endl;
+cout << "======================" << endl;
+
+for (int i = 0; i < n; i++) //Menggunakan perulangan for untuk menyimpan data pada array
+{
+    cout << "Data ke-" << (i + 1) <<":"; //Memasukkan atau menginput nilai data n
+    cin >> arr[i]; //Menyimpan nilai data n kedalam array arr
+}
+}
